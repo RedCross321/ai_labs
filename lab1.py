d1af5377d9928318cae9a1c2d0345ece38b47017
RedCross321 <115776446+RedCross321@users.noreply.github.com>
2024-10-01 12:52:15 +0500

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -1,8 +1,5 @@
 import math
 import random
-
-
-
 class Neurone:
     def __init__(self, n):
         self.number = n
@@ -10,7 +7,7 @@ class Neurone:
         self.somma = 0
     def set_w(self, w = []):
         if len(w) == 0:
-            self.weight = [random.randint(-10,10) for _ in range(self.number)]
+            self.weight = [random.randint(-5,5) for _ in range(self.number)]
         else:
             self.weight = w
     def prevedere(self, x, func):
@@ -22,38 +19,76 @@ class Neurone:
         return func(self.somma)
 
 
+class NeuralNetwork(Neurone):
+    def __init__(self, n, x):
+        self.number = n
+        self.input = x
+        self.dd = []
+        for i in range(self.number):
+            self.dd.append(f'a{i}')
+    def predict(self, inp):
+        #print(self.dd)
+        #inp = [random.randint(1,5) for _ in range(self.input)]
+        
+        for i in range(self.number):
+            self.dd[i] = Neurone(self.input)
+            self.dd[i].set_w()
+            print(inp,"", self.dd[i].prevedere(inp, chet), self.dd[i].weight)
+
 def impl(a):
     return 0 if a > 0 else 1
 
 def cos(a):
     return math.cos(a)
 
-x = int(input("Enter number of inputs -> "))
-dd = Neurone(x)
+def chet(a):
+    return a
 
-def relu(a):
-    if a > 0 :
-        return a
-    else:
-        return 0
+inp = [1, 3]
+
+Neur_n = int(input("Enter number of neurone -> "))
+num_input = int(input("Enter number of input -> "))
+run = NeuralNetwork(Neur_n, num_input)
+
+run.predict(inp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def relu(a):
+#     if a > 0 :
+#         return a
+#     else:
+#         return 0
 
     
-X = [[] for i in range(100)]
-I = 0
+# X = [[] for i in range(100)]
+# I = 0
 
-for i in X:
-    I+=1
-    i.append(math.sin(math.pi * 2 + math.pi/4 + I*I) + math.pi )
-    i.append(I)
-    i.append(I * math.sin(math.pi*2+math.pi/2 + I))
+# for i in X:
+#     I+=1
+#     i.append(math.sin(math.pi * 2 + math.pi/4 + I*I) + math.pi )
+#     i.append(I)
+#     i.append(I * math.sin(math.pi*2+math.pi/2 + I))
 
-dd.set_w([1, 1, 1])
+# dd.set_w([1, 1, 1])
 
-for i in range(100):
-    print(X[i],"  ",dd.prevedere(X[i],relu))
+# for i in range(100):
+#     print(X[i],"  ",dd.prevedere(X[i],relu))
 
-for _ in range(10):
-    inp = [random.randint(0,1) for _ in range(x)]
-    print(inp,"",len(inp),dd.prevedere(inp, impl))
+# for _ in range(10):
+#     inp = [random.randint(0,1) for _ in range(x)]
+#     print(inp,"",len(inp),dd.prevedere(inp, impl))
 
 
